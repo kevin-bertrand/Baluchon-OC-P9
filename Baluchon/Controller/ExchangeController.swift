05c2ecae8f2d5dd7640525f0709d7d0192385aad
Kevin Bertrand <k.bertrand@desyntic.com>
2022-04-25 16:55:34 +0200

    Adding keyboard dismiss to the exchange view

--- Baluchon/Controller/ExchangeController.swift
+++ Baluchon/Controller/ExchangeController.swift
@@ -17,6 +17,7 @@ class ExchangeController: UIViewController, UITableViewDelegate, UITableViewData
         super.viewDidLoad()
         tableView.delegate = self
         tableView.dataSource = self
+        setupTextFields()
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -39,5 +40,21 @@ class ExchangeController: UIViewController, UITableViewDelegate, UITableViewData
         return exchangeCell
     }
     
+    func setupTextFields() {
+        let toolbar = UIToolbar()
+        let flexSpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace,
+                                        target: nil, action: nil)
+        let doneButton = UIBarButtonItem(title: "Done", style: .done,
+                                         target: self, action: #selector(doneButtonTapped))
+        
+        toolbar.setItems([flexSpace, doneButton], animated: true)
+        toolbar.sizeToFit()
+        
+        moneyToExchangeField.inputAccessoryView = toolbar
+    }
+    
+    @objc func doneButtonTapped() {
+        view.endEditing(true)
+    }
     
 }
