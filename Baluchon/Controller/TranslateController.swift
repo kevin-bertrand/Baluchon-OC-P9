f2f65eefa74fd22a6fe0d9c15015a8b927b3d969
Kevin Bertrand <k.bertrand@desyntic.com>
2022-04-25 17:29:34 +0200

    Adding placeholder to the "Text to translate" view

--- Baluchon/Controller/TranslateController.swift
+++ Baluchon/Controller/TranslateController.swift
@@ -7,18 +7,29 @@
 
 import UIKit
 
-class TranslateController: UIViewController {
+class TranslateController: UIViewController, UITextViewDelegate {
     // MARK: Public
     // MARK: Outlers
     @IBOutlet weak var textToTranslateView: TopRoundedTextView!
     @IBOutlet weak var translatedTextView: BottomRoundedTextView!
+    @IBOutlet weak var placeholderLabel: UILabel!
     
     // MARK: Initialisation function
     override func viewDidLoad() {
         super.viewDidLoad()
+        textToTranslateView.delegate = self
     }
     
     // MARK: Actions
     @IBAction func translateButtonTouched() {
+    }
+    
+    // MARK: Methods
+    func textViewDidChange(_ textView: UITextView) {
+        if textToTranslateView.text.isEmpty {
+            placeholderLabel.isHidden = false
+        } else {
+            placeholderLabel.isHidden = true
+        }
     }
 }
