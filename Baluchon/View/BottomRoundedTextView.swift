81a910a9ba31002dfb1991699e852e2a2f3acc39
Kevin Bertrand <k.bertrand@desyntic.com>
2022-04-27 16:23:03 +0200

    Modify display of the translation

--- Baluchon/View/BottomRoundedTextView.swift
+++ Baluchon/View/BottomRoundedTextView.swift
@@ -11,6 +11,7 @@ class BottomRoundedTextView: UITextView {
 
     override func draw(_ rect: CGRect) {
         super.draw(rect)
+        self.contentInset = .init(top: 10, left: 10, bottom: 0, right: 0) // Add margin to the text inside the view
         self.layer.cornerRadius = 25
         self.layer.maskedCorners = [.layerMinXMaxYCorner , .layerMaxXMaxYCorner]
     }
